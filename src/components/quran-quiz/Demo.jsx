32499a7a5f6b49579ba9f5b15523d2fec595357f
SAFAR ABBAS <safarabbas73.sa@gmail.com>
2023-05-26 20:10:54 +0300

    time update

--- src/components/quran-quiz/Demo.jsx
+++ src/components/quran-quiz/Demo.jsx
@@ -4,7 +4,7 @@ const SooratKausar = () => {
   return (
     <div className="dateAdded">
       <h1>The Quiz access is <span>denied Now</span>!</h1><h1>The Quiz will open on 28 May 2023 at 12 to 15 Pakistan Time</h1>
-      <p>انتظار فرمائیں۔ یہ سوالنامہ 28 مئی بروز اتوار پاکستان ٹائم کے مطابق 12 بجے سے لے کر شام 5 بجے تک اوپن رہے گا</p>
+      <p>انتظار فرمائیں۔ یہ سوالنامہ 28 مئی بروز اتوار پاکستان ٹائم کے مطابق 12 بجے سے لے کر شام 3 بجے تک اوپن رہے گا</p>
     </div>
   );
 };
