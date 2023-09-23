1636b0387947a5d666ca06e92f414acfcc989603
SAFAR ABBAS <safarabbas73.sa@gmail.com>
2023-09-23 08:29:16 +0300

    safar

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -43,7 +43,7 @@ function App() {
   const currentHour = currentDate.getUTCHours() + 5;
   // test date and time
 
-  const targetDate = 23;
+  const targetDate = 24;
 
   const from = 9;
   const to = 21;
