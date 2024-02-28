b7c6d866abc084b9225e1c28bfdac10c1093047a
SAFAR ABBAS <safarabbas73.sa@gmail.com>
2024-02-28 06:10:48 +0300

    soorat ibrahim

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -44,9 +44,9 @@ function App() {
   const currentHour = currentDate.getUTCHours() + 5;
   // test date and time
 
-  const targetDate = 18;
-  const EnglishMonth = "FEBRUARY";
-  const UrduMonth = "فروری";
+  const targetDate = 3;
+  const EnglishMonth = "MARCH";
+  const UrduMonth = "مارچ";
 
   const from = 9;
   const to = 21;
