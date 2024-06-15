71c320bfd3b0409e43f077d67365d31b9baa395f
SAFAR ABBAS <safarabbas73.sa@gmail.com>
2024-06-15 05:30:48 +0300

    quiz access

--- src/components/quran-quiz/Access.jsx
+++ src/components/quran-quiz/Access.jsx
@@ -1,10 +1,13 @@
-import React from "react";
+// import React from "react";
 
 const QuizAccess = (props) => {
   return (
     <div className="dateAdded">
-      <h1>The Quiz access is <span>denied Now</span>!</h1><h1>This Quiz will open on {props.date} {props.EnglishMonth} 2023 at {props.from}AM to {props.to}PM Pakistan Time</h1>
-      <p className="text">انتظار فرمائیں۔ یہ سوالنامہ {props.date} {props.UrduMonth}  پاکستان ٹائم کے مطابق صبح  {props.from} بجے سے لے کر رات {props.to} بجے تک اوپن رہے گا</p>
+      <h1>
+        The Quiz Access is <span>denied Now!</span>
+      </h1>
+      {/* <h1>The Quiz access is <span>denied Now</span>!</h1><h1>This Quiz will open on {props.date} {props.EnglishMonth} 2023 at {props.from}AM to {props.to}PM Pakistan Time</h1> */}
+      {/* <p className="text">انتظار فرمائیں۔ یہ سوالنامہ {props.date} {props.UrduMonth}  پاکستان ٹائم کے مطابق صبح  {props.from} بجے سے لے کر رات {props.to} بجے تک اوپن رہے گا</p> */}
     </div>
   );
 };
